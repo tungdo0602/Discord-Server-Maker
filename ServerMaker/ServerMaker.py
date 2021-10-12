d5544c2b2bafa261924b4204e869ccb606a122a2
tungdo0602 <89498812+tungdo0602@users.noreply.github.com>
2021-10-12 11:08:52 +0700

    Add files via upload

---
+++ ServerMaker/ServerMaker.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+file = open("ServerConfig.txt", "r")
+server_template = file.read()
+
+token = ""
+
+maker = requests.post('https://discord.com/api/v9/guilds', headers={'authorization': token}, json=server_template
+if maker.status_code == 201:
+	print("Successfully make server")
+else:
+	print(f"Failed ERROR {maker.status_code}")
